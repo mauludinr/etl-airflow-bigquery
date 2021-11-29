c966964586403afe97b77ec4d32eae4e819daa16
M Mauludin Rohman <38213112+mauludinr@users.noreply.github.com>
2021-11-29 21:27:15 +0700

    Create bq_dag.py

---
+++ dags/bq_dag.py
@@ -0,0 +1,31 @@
+import os
+from airflow import DAG
+from airflow import configuration
+from airflow.models import Variable
+from datetime import datetime
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.bash import BashOperator
+
+default_args = {
+    'owner': 'mauludinr',
+    'start_date': datetime(2021,11,26),
+    'retries' : 1,
+}
+
+dag= DAG(
+    dag_id = "dag_bq_to_bq",
+    default_args=default_args,
+    catchup=False,
+    #Because i want to start this at 08:00 PM UTC at airflow timezone, i need to set the schedule to 03:00 AM based on my local timezone in WIB(+07:00) 
+    schedule_interval='0 03 * * *',
+    tags=['ecommerce']
+)
+
+with dag:
+
+    task_bash= BashOperator(
+    	task_id='bq_to_bq',
+    	bash_command='sh /home/mauludinr/run_bq.sh ',
+     )
+		
+     task_bash
